e7853f2a5435eac69b195f7c1e1b9a7f9f677d3d
Daniel Araujo <daniel-fonseca13@hotmail.com>
2019-06-19 17:15:14 -0300

    Cadastro de pessoa juridica concluido

--- public/js/pessoaJuridica.js
+++ public/js/pessoaJuridica.js
@@ -1,37 +1,37 @@
 var stringInput = document.getElementById('string-input');
 
 var database = firebase.database();
-function novaPessoaFisica(){
+function novaPessoaJuridica(){
     var nome = document.getElementById('inputName').value;
+    var razaoSocial = document.getElementById('inputRazao').value;
     var email = document.getElementById('inputEmail').value;
     var senha = document.getElementById('inputPassword').value;
     var telefoneCel = document.getElementById('inputTelefoneCel').value;
     var telefone = document.getElementById('inputTelefone').value;
-    var dataNasc = document.getElementById('inputDataNasc').value;
-    var cpf = document.getElementById('inputCPF').value;
-    var sexo = document.getElementById('inputSexo').value;
-    var estadoCivil = document.getElementById('inputEstadoCivil').value;
+    var inscricaoEstadual = document.getElementById('inputInscEstadual').value;
+    var cnpj = document.getElementById('inputCNPJ').value;
+    var ramoAtividade = document.getElementById('inputRamo').value;
     var rua = document.getElementById('inputStreet').value;
     var nmrResidencial = document.getElementById('inputNumber').value;
     var bairro = document.getElementById('inputNeighborhood').value;
     var cep = document.getElementById('inputCEP').value;
     var cidade = document.getElementById('inputCity').value;
     var estado = document.getElementById('inputState').value;
-    if(nome == "" || email == "" || senha == "" || telefoneCel == "" || telefone == "" || dataNasc == "" || cpf == "" || sexo == "" 
-    || estadoCivil == "" || rua == "" || nmrResidencial == "" || bairro == "" || cep == "" || cidade == "" || estado == ""){
+    if(nome == "" || razaoSocial == "" || email == "" || senha == "" || telefoneCel == "" || telefone == "" || inscricaoEstadual == "" || cnpj == "" || ramoAtividade == "" 
+    || rua == "" || nmrResidencial == "" || bairro == "" || cep == "" || cidade == "" || estado == ""){
         alert('Preencha todos os campos!')
     }
     else{
-        firebase.database().ref('usuarioFisico/').push().set({
+        firebase.database().ref('usuarioJuridico/').push().set({
             nome: document.getElementById('inputName').value,
+            razaoSocial: document.getElementById('inputRazao').value,
             email: document.getElementById('inputEmail').value,
             senha: document.getElementById('inputPassword').value,
             telefoneCel: document.getElementById('inputTelefoneCel').value,
             telefone: document.getElementById('inputTelefone').value,
-            dataNasc: document.getElementById('inputDataNasc').value,
-            cpf: document.getElementById('inputCPF').value,
-            sexo: document.getElementById('inputSexo').value,
-            estadoCivil: document.getElementById('inputEstadoCivil').value,
+            inscricaoEstadual: document.getElementById('inputInscEstadual').value,
+            cnpj: document.getElementById('inputCNPJ').value,
+            ramoAtividade: document.getElementById('inputRamo').value,
             rua: document.getElementById('inputStreet').value,
             nmrResidencial: document.getElementById('inputNumber').value,
             bairro: document.getElementById('inputNeighborhood').value,
@@ -56,7 +56,7 @@ function createLogin(){
 }
 
 var executa = function(){
-    novaPessoaFisica();
+    novaPessoaJuridica();
     createLogin();
 }
 
